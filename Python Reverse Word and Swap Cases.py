49f53cab134fa024b8b64ee3fa7857ae7646c95a
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2020-06-20 18:49:05 +0530

    Add files via upload

---
+++ Python Reverse Word and Swap Cases.py
@@ -0,0 +1,34 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'reverse_words_order_and_swap_cases' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING sentence as parameter.
+#
+
+def reverse_words_order_and_swap_cases(sentence):
+    word_list = sentence.split()
+    reversed_list = word_list[:: -1]
+    reversed_sentence = " ".join(reversed_list)
+    return reversed_sentence.swapcase()
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    sentence = raw_input()
+
+    result = reverse_words_order_and_swap_cases(sentence)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
